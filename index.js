b442a58238fb33fd31b8c5a2b94d8b2b9f121bf2
David Silva <davidsilvav6@gmail.com>
2024-01-06 12:04:39 -0500

    segundo Testeo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
-
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const navToggle = document.querySelector(".nav-toggle");
+    const links = document.querySelector(".links");
+  
+    navToggle.addEventListener("click", function () {
+      console.log("Click event");
+      links.classList.toggle("show-links");
+    });
+  });
