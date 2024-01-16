731a4ab0e8831e9f70f2b66b430be75b283d2496
David Silva <davidsilvav6@gmail.com>
2024-01-16 10:27:28 -0500

    barra busqueda

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
-
-
     const navToggle = document.querySelector(".nav-toggle");
     const links = document.querySelector(".links");
   
@@ -8,10 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log("Click event");
       links.classList.toggle("show-links");
     });
-
-
-
-
-
-    
   });
+
+
+
+const alerta = document.querySelector(".botton-precio");
+alert.addEventListener("click",mostrarAlerta)
+
+function mostrarAlerta(){
+  alert("Hola mundo")
+}
+
+
+
+/*arrays para los productos*/
+let cortinas = ["id","nombre","precio", "color"] {
+
+}
+
+let persianas =  ["id","nombre","precio", "color" ] {
+
+}
